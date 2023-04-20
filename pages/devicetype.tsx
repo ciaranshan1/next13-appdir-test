a23b41f71d6e32dbd00c4a2852f7fb4834c3cb61
Ciaran Shanahan <52163264+ciaranshan1@users.noreply.github.com>
2023-04-20 10:40:02 +0100

    feat: add device type response header

--- pages/devicetype.tsx
+++ pages/devicetype.tsx
@@ -1,8 +1,10 @@
 import { GetServerSideProps } from "next";
+ import { IncomingHttpHeaders } from "http";
 
 // Define a type for our product data
 type Product = {
   name: string | string[] | undefined;
+  headers: string[]
 };
 
 // Define a type for our props
@@ -16,6 +18,7 @@ export default function productsPage({ product }: PropductProps) {
   return (
     <div style={{ height: "100vh", padding: 50 }}>
       <h1 style={{ color: "red" }}>Device Type: {product.name}</h1>
+      <p>Headers: {product.headers} </p>
     </div>
   );
 }
@@ -31,15 +34,26 @@ export const getServerSideProps: GetServerSideProps<{
   const { req } = context;
   let devicetype = req.headers["x-atlas-device-type"];
   console.log(devicetype);
-
+  
   if (devicetype == undefined) {
     devicetype = "notset";
+  }
+
+  context.res.setHeader(
+    "x-atlas-device-type",
+    devicetype
+  );
+
+  let hrs = []
+  for (var k in req.headers) {
+    hrs.push(k.toString())
   }
 
   return {
     props: {
       product: {
         name: devicetype,
+        headers: hrs
       },
     },
   };
